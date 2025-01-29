4ff7e5db3fc7e8c52bc677360f7169c93003c2a0
wfqplusplus <2962702667@qq.com>
2025-01-29 12:54:54 +0800

    解决一个死锁问题 资源释放重构

--- 2_ThreadPool cached/main.cpp
+++ 2_ThreadPool cached/main.cpp
@@ -100,26 +100,26 @@ private:
 
 int main()
 {
-    //{
-    //    ThreadPool pool;
-    //    pool.setMode(PoolMode::MODE_CACHED);
-    //    // 开始启动线程池
-    //    pool.start(2);
+    {
+        Thread_Pool pool;
+        pool.setMode(PoolMode::MODE_CACHED);
+        // 开始启动线程池
+        pool.start(2);
 
-    //    // linux上，这些Result对象也是局部对象，要析构的！！！
-    //    Result res1 = pool.submitTask(std::make_shared<MyTask>(1, 100000000));
-    //    Result res2 = pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
-    //    pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
-    //    pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
-    //    pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        // linux上，这些Result对象也是局部对象，要析构的！！！
+        Result res1 = pool.submitTask(std::make_shared<MyTask>(1, 100000000));
+        Result res2 = pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
 
-    //    //uLong sum1 = res1.get().cast_<uLong>();
-    //    //cout << sum1 << endl; 
-    //} // 这里Result对象也要析构!!! 在vs下，条件变量析构会释放相应资源的
-    //
-    //cout << "main over!" << endl;
-    //getchar();
-#if 1
+        //uLong sum1 = res1.get().cast_<uLong>();
+        //cout << sum1 << endl; 
+    } // 这里Result对象也要析构!!! 在vs下，条件变量析构会释放相应资源的
+    
+    cout << "main over!" << endl;
+    getchar();
+#if 0
     // 问题：ThreadPool对象析构以后，怎么样把线程池相关的线程资源全部回收？
     {
         Thread_Pool pool;
---
+++ 3_ThreadPool cached  死锁问题/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+#include "Thread_Pool.h"
+
+/*
+有些场景，是希望能够获取线程执行任务得返回值得
+举例：
+1 + 。。。 + 30000的和
+thread1  1 + ... + 10000
+thread2  10001 + ... + 20000
+.....
+
+main thread：给每一个线程分配计算的区间，并等待他们算完返回结果，合并最终的结果即可
+*/
+
+using uLong = unsigned long long;
+
+class MyTask : public Task
+{
+public:
+    MyTask(int begin, int end)
+        : begin_(begin)
+        , end_(end)
+    {}
+    // 问题一：怎么设计run函数的返回值，可以表示任意的类型
+    // Java Python   Object 是所有其它类类型的基类
+    // C++17 Any类型
+    Any run()  // run方法最终就在线程池分配的线程中去做执行了!
+    {
+        std::cout << "tid:" << std::this_thread::get_id()
+            << "begin!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        uLong sum = 0;
+        for (uLong i = begin_; i <= end_; i++)
+            sum += i;
+        std::cout << "tid:" << std::this_thread::get_id()
+            << "end!" << std::endl;
+
+        return sum;
+    }
+
+private:
+    int begin_;
+    int end_;
+};
+
+int main()
+{
+    {
+        Thread_Pool pool;
+        pool.start();
+        Result res = pool.submitTask(std::make_shared<MyTask>(100000001, 200000000));
+        uLong sum1 = res.get().cast_<uLong>();
+        cout << sum1 << endl;
+    }
+    cout << "main over!" << endl;
+    getchar();
+    return 0;
+}
