e6cb370f58f05f52d426b34e5af7e348f17e5846
wfqplusplus <2962702667@qq.com>
2025-01-29 14:42:39 +0800

    补充信号量解决平台差异性

--- 3_ThreadPool cached  死锁问题/Thread_Pool.h
+++ 3_ThreadPool cached  死锁问题/Thread_Pool.h
@@ -70,13 +70,19 @@ class Semaphore
 {
 public:
 	Semaphore(int limit = 0)
-		:resLimit_(limit)
+		:resLimit_(limit), isExit_(false)
 	{}
-	~Semaphore() = default;
+
+	~Semaphore()
+	{
+		isExit_ = true;
+	}
 
 	// 获取一个信号量资源
 	void wait()
 	{
+		if (isExit_)
+			return;
 		std::unique_lock<std::mutex> lock(mtx_);
 		// 等待信号量有资源，没有资源的话，会阻塞当前线程
 		cond_.wait(lock, [&]()->bool {return resLimit_ > 0; });
@@ -86,6 +92,8 @@ public:
 	// 增加一个信号量资源
 	void post()
 	{
+		if (isExit_)
+			return;
 		std::unique_lock<std::mutex> lock(mtx_);
 		resLimit_++;
 		// linux下condition_variable的析构函数什么也没做
@@ -93,6 +101,7 @@ public:
 		cond_.notify_all();  // 等待状态，释放mutex锁 通知条件变量wait的地方，可以起来干活了
 	}
 private:
+	std::atomic_bool isExit_;  // 考虑平台差异性
 	int resLimit_;
 	std::mutex mtx_;
 	std::condition_variable cond_;
